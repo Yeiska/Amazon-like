e2e657a8c552182892dc56231f1c725540f04fac
Yeiska <yeiska83@yahoo.com>
2018-08-10 22:46:38 -0400

    customer table working

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Amazon-like
+
+## Synopsis
+
+This is a fictitious online store inventory database that allows users to select a product and 'buy' a quantity of that item. The MySQL database get's updated when the user chooses to buy products. This app utilizes Node.js, MySQL and [Inquirer](https://www.npmjs.com/package/inquirer/).
+
+
+## Code example
+
+Upon running the program, the user will be shown all of the inventory in the database, and will be asked to give the ID of the product they would like to purchase:
+
+![run app](images/inventory.gif)
+
+Once the user provides the product ID, they are asked how many units of the product they would like to buy. Once they enter the amount, if the number that they entered is less than or equal to the current inventory number for the selected product, then the inputted number is subtracted from the product's available quantity and the product's new quantity is updated in the MySQL database. The user is shown a message stating that the products have been bought along with the total cost of their transaction. They are then asked whether they would like to shop again.
+
+If the user types in a quantity that is larger than the current available quantity for the product that they have chosen, then an error message pops up, and ask then if they want to purchase another item, if it is confirm they  are taken back to the starting prompt otherwise show a messaje saying ..See you soon!
+
+![updated in the database](images/buying.gif)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,33 +22,71 @@ connection.connect(function (err) {
     showCustomer();
 });
 
+var x;
 function showCustomer() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
         // Log all results of the SELECT statement
-        console.log(res);
-        select();
-    });
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+            x = parseInt(res[i].stock_quantity);
+        }
+        questions = [
+            {
+                name: "item_id",
+                type: "input",
+                message: "Which is the id for the item you would like to buy?"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?"
+            }
+        ]
+        inquirer.prompt(questions).then(function (res1) {
+            var select = (res1.item_id) - 1;
+            var qty = parseInt(res1.quantity);
+            var total = parseFloat((res[select].price + qty).toFixed(2));
+            if (x < qty) {
+                console.log(x);
+                console.log("Sorry Insuficiente on stock!!");
+                return reprompt();
+            } else {
+                              
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: ((res.stock_quantity) - qty)
+                        },
+                        {
+                            id: res1.id
+                        }
+                    ], function (err, res2) {
+                        console.log("Success! Your total is $" + total.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
+                        reprompt();
+                    }
+                );
+               
+            }            
+        }
+        )
+    }
+    )
 }
 
-questions = [
-    {
-        name: "id",
-        type: "input",
-        message: "Which is the id for the item you would like to buy?"
-    },
-    {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to buy?"
-    }
-]
-function select() {
-    inquirer.prompt(questions).then(function () {
-        var query = "SELECT id FROM products where products.id = ?";
-        connection.query( query, function (err, res) {
-            console.log(res);
-        })
+
+function reprompt(){
+    inquirer.prompt([{
+      type: "confirm",
+      name: "reply",
+      message: "Would you like to purchase another item?"
+    }]).then(function(ans){
+      if(ans.reply){
+        showCustomer();
+      } else{
+        console.log("See you soon!");
+      }
     });
-}
+  }
+  
